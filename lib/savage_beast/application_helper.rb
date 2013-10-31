456fa6ef2752e7e4c3331b9bc0e632b2b84e22ec
quyen <nttqa22001@yahoo.com>
2013-10-31 17:40:14 +0100

    fix no returning method

--- lib/savage_beast/application_helper.rb
+++ lib/savage_beast/application_helper.rb
@@ -41,7 +41,8 @@ module SavageBeast
 		end
 
 		def search_posts_title
-			returning(params[:q].blank? ? 'Recent Posts'[] : "Searching for"[] + " '#{h params[:q]}'") do |title|
+			title = params[:q].blank? ? 'Recent Posts'[] : "Searching for"[] + " '#{h params[:q]}'"
+      			title.tap do |title|
 				title << " "+'by {user}'[:by_user,h(User.find(params[:user_id]).display_name)] if params[:user_id]
 				title << " "+'in {forum}'[:in_forum,h(Forum.find(params[:forum_id]).name)] if params[:forum_id]
 			end
